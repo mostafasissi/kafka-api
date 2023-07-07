afdc6216a365215115affed38a2fa06eb24adec4
Mostafa Sissi <mostafasissi@gmail.com>
2023-07-07 12:39:19 +0100

    first-commit : create kafka topic

---
+++ src/main/java/ehtp/mostafa/email_web_service/EmailWebServiceApplication.java
@@ -0,0 +1,25 @@
+package ehtp.mostafa.email_web_service;
+
+import ehtp.mostafa.email_web_service.entities.ProducerMessage;
+import ehtp.mostafa.email_web_service.services.ProductService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EmailWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWebServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(ProductService productService){
+		return args -> {
+
+			productService.sendMessage("first message");
+		};
+	}
+
+}
---
+++ src/test/java/ehtp/mostafa/email_web_service/EmailWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ehtp.mostafa.email_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
